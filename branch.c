e94ec625d94462e198290be51080ff2e24f6eca9
Nick House <nh6gy@umsystem.edu>
2021-02-15 18:59:47 -0600

    Added timer, passed time to child process. Fixed open file validation for integers and created shared memory.

--- branch.c
+++ branch.c
@@ -6,7 +6,7 @@ int main(int argc, char * argv[]) {
 
 //Testing Output using Fork and execl 
 
-	printf("%s: %s\n", argv[0], argv[1]); 
+	printf("%s: %s Time: %d", argv[0], argv[2], argv[1]); 
 
 	return EXIT_SUCCESS; 
 }
--- master.h
+++ master.h
@@ -10,4 +10,5 @@ struct sharedMemory {
 	int datanumber; 
 };
 
+
 #endif
