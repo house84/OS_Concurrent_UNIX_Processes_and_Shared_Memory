df18f52cdc0707965f463b4558a5133a7fe13d1d
Nick House <nh6gy@umsystem.edu>
2021-02-23 14:35:49 -0600

    All Processes Look to be working, add math condition to sum values remaining

--- bin_adder.c
+++ bin_adder.c
@@ -17,6 +17,7 @@ void critical_section();		//Critical Part to avoid race condition
 struct sharedMemory *shmptr; 	//Global shared memory pointer
 int xx; 
 int yy; 
+int procNum; 
 
 int main(int argc, char * argv[], char * envp[]) {
 
@@ -27,15 +28,28 @@ int main(int argc, char * argv[], char * envp[]) {
 	//Set shmptr from shmid
 	shmptr = (struct sharedMemory *) shmat(atoi(argv[3]), NULL, 0);  
 	
+	//argv[1] procNum%20 
+	procNum = atoi(argv[1])%20;  
+	
 	//argv[1] xx
 	xx = atoi(argv[1]); 
 
 	//argv[2] yy
 	yy = atoi(argv[2]); 
 
+	fprintf(stderr, "ProcNum: %d\n", procNum); 
 	//Call Solution 4
-	process(xx); 
+	process(procNum); 
 	
+	//Testing
+//	critical_section(); 
+
+	//release Memory Ptr
+	shmdt(shmptr); 
+	
+	//test
+	fprintf(stderr, "Child Process Exit\n"); 
+
 	return EXIT_SUCCESS;
 }
 
@@ -45,33 +59,48 @@ int main(int argc, char * argv[], char * envp[]) {
 //===Multi Process Solution 4===//
 void process ( const int i ) {
 
-		int j; 		//Local to each process
-		int n; 		//Number of Processes Total
-		n = 19; 		
+		int j = NULL; 												//Local to each process
+		int n = 20; 													//Number of Processes
+		
+		//Ensure Current Index is not 
+		//being used by previous Process
+		while( shmptr->flag[i] != idle ){
 
-		do {
+			wait(NULL); 		
+		}	
 	
-			shmptr->flag[i] = want_in; 	//raise my flag
-			j = shmptr->turn; 			//set local variable
+		do{
+
+			fprintf(stderr, "In Do, J: %d Turn: %d\n", j, shmptr->turn); 
+	
+			shmptr->flag[i] = want_in; 		//raise my flag
+			j = shmptr->turn; 						//set local variable
 				
-			while ( j != i ) 
+			while ( j != i ){
+
 				j = ( shmptr->flag[j] != idle ) ? shmptr->turn : ( j + 1 ) % n; 
 
-			//Declare intention to enter critical section
+			}
 
+			fprintf(stderr, "Post while j != i\n"); 
+			
+			//Declare intention to enter critical section
 			shmptr->flag[i] = in_cs; 
 
 			//Check that no one else is in critical section 
+			for( j = 0; j < n; j++ ){
 
-			for( j = 0; j < n; j++ )
-				if( ( j != i ) && ( shmptr->flag[j] == in_cs ))
+				if( ( j != i ) && ( shmptr->flag[j] == in_cs )){
+
 					break; 
+
+				}
+			}
 			
-		} while ( j < n || ( shmptr->turn != i && shmptr->flag[shmptr->turn] != idle )); 
+		} while (( j < n ) || (( shmptr->turn != i && shmptr->flag[shmptr->turn] != idle ))); 
 	 
 		
 	//Assign turn to self and enter critical section 
-
 	shmptr->turn = i; 
 	
 	//Enter Critical
@@ -82,25 +111,25 @@ void process ( const int i ) {
 	//Exit Section
 	fprintf(stderr, "Exit Critical %d\n", i); 
 
-			
+	//Index to next Array
 	j = ( shmptr->turn + 1 ) % n; 
 
-	while ( shmptr->flag[j] == idle); 
+	while ( shmptr->flag[j] == idle){
+
 		j = ( j + 1 ) % n; 
+	}
 
 	//Assign turn to next waiting process; change flag to idle
 
 	shmptr->turn = j; 
 	shmptr->flag[i] = idle; 
-	 
 }
 
 		
 
 	
-//Exicute Algo
+//Execute Algo in Critical Section
 void critical_section(){
-
 
 	int i;   
 	for(i = 0; i < shmptr->leaves; ++i){
@@ -110,8 +139,5 @@ void critical_section(){
 		printf("[%d] ", shmptr->dataArr[i] ); 
 	}
 	printf("\n"); 	
-
-
-
-	shmptr->currProc--;
+ 
 }
--- shared.h
+++ shared.h
@@ -7,15 +7,14 @@
 enum state {idle, want_in, in_cs};  
 
 struct sharedMemory {
-
+  
 	enum state flag[20]; 
 	int turn; 
 	int dataInputs;  
 	int depth;
-	int leaves;   
-	int currProc;
-//	int *dataArr; 
-	int dataArr[]; 
+	int leaves;    
+	int dataArr[];  
+
 };
 
 
